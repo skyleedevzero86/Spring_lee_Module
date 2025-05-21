997c2b09289647e77d6f4e4a4675486a7709a4e3
이경용 <sleekydz86@icloud.com>
2025-05-22 00:03:37 +0900

    Object 기초편 학습

---
+++ CinemaReservationSystem/src/main/java/com/objectstudy/CinemaReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.objectstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaReservationSystemApplication.class, args);
+    }
+
+}
---
+++ CinemaReservationSystem/src/test/java/com/objectstudy/CinemaReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
