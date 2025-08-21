4faeb6f6dfb9bbd1dba827a6bd2e77aafa6b61c7
이경용 <sleekydz86@icloud.com>
2025-08-21 22:24:54 +0900

    feat: otp_학습 Init (코틀린 버전 만들기전 리마인드)

---
+++ java_otp/src/test/java/com/sleekydz86/JavaOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
