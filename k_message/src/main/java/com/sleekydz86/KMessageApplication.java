ff42622e7dfa6702f1cfcde8f330ab4b75dbda8f
이경용 <sleekydz86@icloud.com>
2025-08-24 21:39:29 +0900

    feat: 알림메세지 기능 프로젝트 셋팅

---
+++ k_message/src/main/java/com/sleekydz86/KMessageApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KMessageApplication.class, args);
+    }
+
+}
