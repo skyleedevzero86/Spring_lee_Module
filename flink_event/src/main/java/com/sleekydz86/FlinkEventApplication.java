1a78562e9700dd2609863bce8ff86dd22c9c5631
이경용 <sleekydz86@icloud.com>
2025-09-03 21:51:45 +0900

    feat:flink 사용자 이벤트 학습 Init

---
+++ flink_event/src/main/java/com/sleekydz86/FlinkEventApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkEventApplication.class, args);
+    }
+
+}
