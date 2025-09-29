4391e7e77571571fdc76182e428aa4df74ffc3d2
이경용 <sleekydz86@icloud.com>
2025-09-29 18:46:07 +0900

    feat: DB연동 학습

---
+++ DB_Study/mysql_version/src/main/java/com/sleekydz86/MysqlVersionApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlVersionApplication.class, args);
+    }
+
+}
---
+++ DB_Study/oracle_version/src/main/java/com/sleekydz86/OracleVersionApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleVersionApplication.class, args);
+    }
+
+}
