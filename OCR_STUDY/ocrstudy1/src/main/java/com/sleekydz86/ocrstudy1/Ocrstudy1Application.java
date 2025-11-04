0c3548d58c96a659168d2a1acc2ce000ca2452dc
이경용 <sleekydz86@icloud.com>
2025-11-04 17:04:04 +0900

    feat: ocr study 학습을 위한 첫번째 미니 프로젝트 셋팅

---
+++ OCR_STUDY/ocrstudy1/src/main/java/com/sleekydz86/ocrstudy1/Ocrstudy1Application.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.ocrstudy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ocrstudy1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ocrstudy1Application.class, args);
+    }
+
+}
---
+++ OCR_STUDY/ocrstudy1/src/test/java/com/sleekydz86/ocrstudy1/Ocrstudy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.ocrstudy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocrstudy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
