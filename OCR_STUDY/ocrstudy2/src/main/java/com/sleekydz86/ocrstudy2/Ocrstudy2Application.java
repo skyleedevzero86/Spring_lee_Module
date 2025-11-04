921b2231e2dd8586dfb3370cf2b32c6925492b4a
이경용 <sleekydz86@icloud.com>
2025-11-04 20:47:22 +0900

    feat: 두번째 버전 python 연동 ocr 학습 프로젝트 셋팅

---
+++ OCR_STUDY/ocrstudy2/src/main/java/com/sleekydz86/ocrstudy2/Ocrstudy2Application.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.ocrstudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ocrstudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ocrstudy2Application.class, args);
+    }
+
+}
---
+++ OCR_STUDY/ocrstudy2/src/test/java/com/sleekydz86/ocrstudy2/Ocrstudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.ocrstudy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocrstudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
