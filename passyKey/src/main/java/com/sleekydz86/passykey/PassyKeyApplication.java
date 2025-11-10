46b19f6d6edbf387e15aa3a0a2ccd4317bc3ce20
이경용 <sleekydz86@icloud.com>
2025-11-10 20:37:53 +0900

    feat: pass학습을 위한 프로젝트 셋팅

---
+++ passyKey/src/main/java/com/sleekydz86/passykey/PassyKeyApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.passykey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassyKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassyKeyApplication.class, args);
+    }
+
+}
