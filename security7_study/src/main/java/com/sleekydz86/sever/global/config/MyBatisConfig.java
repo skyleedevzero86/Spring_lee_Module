5b5274dac8aa777b06c25a039a1e142962ab934e
이경용 <sleekydz86@icloud.com>
2025-12-17 22:49:18 +0900

    feat: 환경설정 추가

---
+++ security7_study/src/main/java/com/sleekydz86/sever/global/config/MyBatisConfig.java
@@ -0,0 +1,32 @@
+package com.sleekydz86.sever.global.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.sleekydz86.sever.model.infrastructure.persistence", sqlSessionFactoryRef = "sqlSessionFactory")
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setMapperLocations(
+            new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml")
+        );
+        sessionFactory.setTypeAliasesPackage("com.sleekydz86.sever.model.domain");
+        
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        sessionFactory.setConfiguration(configuration);
+        
+        return sessionFactory.getObject();
+    }
+}
+
