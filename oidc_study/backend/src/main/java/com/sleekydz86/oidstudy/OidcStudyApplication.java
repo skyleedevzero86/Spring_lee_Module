65e703971e6dfeddfab7839fa3c0bb251971633f
이경용 <sleekydz86@naver.com>
2026-03-20 15:33:00 +0900

    feat: oidc를 학습하기위해 셋팅

---
+++ oidc_study/backend/src/main/java/com/sleekydz86/oidstudy/OidcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.oidstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OidcStudyApplication.class, args);
+    }
+
+}
---
+++ oidc_study/backend/src/test/java/com/sleekydz86/oidstudy/OidcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.oidstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
