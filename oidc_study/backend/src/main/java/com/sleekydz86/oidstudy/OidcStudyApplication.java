86d06011e69e084324607035db4f9aa783f8fbd6
이경용 <sleekydz86@naver.com>
2026-03-22 19:51:32 +0900

    chore: Spring Boot Gradle 프로젝트 및 진입점 추가
    - 애플리케이션 진입점과 런타임 설정(Oracle·Naver OIDC placeholder) 추가

--- oidc_study/backend/src/main/java/com/sleekydz86/oidstudy/OidcStudyApplication.java
+++ oidc_study/backend/src/main/java/com/sleekydz86/oidstudy/OidcStudyApplication.java
@@ -2,8 +2,10 @@ package com.sleekydz86.oidstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OidcStudyApplication {
 
     public static void main(String[] args) {
