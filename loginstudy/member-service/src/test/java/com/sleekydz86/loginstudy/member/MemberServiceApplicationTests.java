b5c2df74c77f668a853e94cf988144fbdc23755e
이경용 <sleekydz86@naver.com>
2026-07-15 17:48:53 +0900

    feat: dockercompose와 DB구성
    - 트랜잭션처리 Flyway 기능구현

--- loginstudy/member-service/src/test/java/com/sleekydz86/loginstudy/member/MemberServiceApplicationTests.java
+++ loginstudy/member-service/src/test/java/com/sleekydz86/loginstudy/member/MemberServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.sleekydz86.loginstudy.member;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class MemberServiceApplicationTests {
 
