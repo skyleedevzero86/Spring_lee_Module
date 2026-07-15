6765b04850ac0701f94cde0f4764052e63b2d569
이경용 <sleekydz86@naver.com>
2026-07-15 18:51:46 +0900

    fix:  auth-server Authorization Server 기본 구현하기

--- loginstudy/auth-server/src/test/java/com/sleekydz86/loginstudy/auth/AuthServerApplicationTests.java
+++ loginstudy/auth-server/src/test/java/com/sleekydz86/loginstudy/auth/AuthServerApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class AuthServerApplicationTests {
+class AuthServerApplicationTests extends AuthServerIntegrationTestSupport {
 
 	@Test
 	void contextLoads() {
